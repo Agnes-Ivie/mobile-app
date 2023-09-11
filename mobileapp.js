fec3903344e8b85619b24cc6f3c3d9e1112acc4f
codera001 <veranwoye9@gmail.com>
2023-09-11 14:29:38 +0100

    nav link

--- mobileapp.js
+++ mobileapp.js
@@ -2,5 +2,5 @@ const bar= document.querySelector('.bar');
 const navLink = document.querySelector('.nav-link');
 
 bar.addEventListener('click', () => {
-  navLink.classList.toggle('nav-link');
+  navLink.classList.toggle('hide');
 });
